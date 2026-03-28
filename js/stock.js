7a7b38c5e13a6db39e39782117fc72c20103b2f6
Nicolas <nicolascosta15112004@gmail.com>
2026-03-28 07:26:23 -0300

    actualizar version json
    Se actualiza la versión para forzar al buscador a leerlo

--- js/stock.js
+++ js/stock.js
@@ -2,7 +2,7 @@
 // Use async/await para obtener los productos del JSON 
 const obtenerProductos = async () => {
     try {
-        const response = await fetch("./data/productos.json"); 
+        const response = await fetch("./data/productos.json?v=1.0"); 
         const data = await response.json(); 
         return data;
     } catch (error) {
